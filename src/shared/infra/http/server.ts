a61ac2b9709fc7c3b69472e4f3c0aa111e6b1d22
Henrique Lopes <henrique.henriquelopess@gmail.com>
2020-10-17 22:01:25 -0300

    Lidando com possíveis erros

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import Routes from './routes';
 import path from 'path';
+import ErrorHandler from '@shared/errors/handler';
+import 'express-async-errors';
 
 import "../typeorm/connection";
 
@@ -9,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(Routes);
 app.use("/upload", express.static(path.join(__dirname, "..","..","..", "upload")));
+app.use(ErrorHandler);
 
 app.listen(3333, ()=>{
     console.log("Server started on port 3333");
